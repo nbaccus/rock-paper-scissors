942d30771442fa06c18565e494c7e91dea34afe0
Reeni <nazireen.imambaccus@gmail.com>
2021-06-20 19:37:53 +0100

    removed unused function

--- js/script.js
+++ js/script.js
@@ -32,22 +32,22 @@ function playRound() {
 	const para1 = document.createElement('p');
 
     if (playerSelection == computerSelection) {
-	para1.textContent= `ROUND ${count}: It's a tie! You played ${playerSelection} and Computer played ${computerSelection}.`;
-	display.appendChild(para1);
+	    para1.textContent= `ROUND ${count}: It's a tie! You played ${playerSelection} and Computer played ${computerSelection}.`;
+	    display.appendChild(para1);
     }
     else if (
         playerSelection == "scissors" && computerSelection == "paper"
     || playerSelection == "rock" & computerSelection == "scissors"
     || playerSelection == "paper" & computerSelection == "rock" 
     ) {
-	para1.textContent= `ROUND ${count}: You win! You played ${playerSelection} and Computer played ${computerSelection}.`;
-	display.appendChild(para1);
-	wins+=1;       
+	    para1.textContent= `ROUND ${count}: You win! You played ${playerSelection} and Computer played ${computerSelection}.`;
+	    display.appendChild(para1);
+	    wins+=1;       
     }
     else {
-	para1.textContent= `ROUND ${count}: You lose... You played ${playerSelection} and Computer played ${computerSelection}.`;
-	display.appendChild(para1);
-	losses+=1;
+	    para1.textContent= `ROUND ${count}: You lose... You played ${playerSelection} and Computer played ${computerSelection}.`;
+	    display.appendChild(para1);
+	    losses+=1;
     }
 
 
@@ -69,40 +69,6 @@ function playRound() {
 }
 
 
-
-
-function game() {
-    let roundNumber = 1;
-    var playerWin =0;
-    var playerTies=0;
-    var playerLoss=0;
-    for (i =1; i <= roundNumber; i++) {
- 
-        if (playerSelection == null || playerSelection == "") {
-            return null;
-        }
-        var computerSelection = computerPlay();
-        var result = (playRound(playerSelection, computerSelection));
-        if (result ==1) { 
-            playerWin += 1;
-        }
-        if (result==-1) {
-            playerLoss +=1;
-        }
-        else {
-            playerTies +=1;
-        }
-    }
-    if (playerWin > playerLoss) {
-        console.log(`You are the winner! You have won ${playerWin} time(s).`);
-    }
-    else if (playerWin == playerLoss) {
-        console.log(`It's a tie!`);
-    }
-    else {
-        console.log(`You've lost...You have lost ${playerLoss} time(s).`);
-    }
-}
 let buttons = document.querySelectorAll('button');
 buttons.forEach (button => {
 	if (button.id != "roundsbtn" && button.id!="restart" ) {
@@ -125,14 +91,6 @@ rndbtn.addEventListener('click', () => {
 });
 });
 
-/*
-let buttons = document.querySelectorAll('button');
-buttons.forEach (button => {
-	if (button.id != "roundsbtn" ) {
-		button.addEventListener('click', playRound);	
-	}
-});
-*/
 
 function disableButtons() {
 let buttons = document.querySelectorAll('button');
